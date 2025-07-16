35f431f26396d5615eaa6d9346cc275e55b9618f
jhallj <julien-hall@outlook.fr>
2025-07-16 22:49:10 +0200

    Add files via upload

---
+++ JHA_TPMoyennes/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    public static class Utils
+    {
+        public static double TruncateToTwoDecimals(double value)
+        {
+            return Math.Truncate(value * 100) / 100;
+        }
+    }
+}
