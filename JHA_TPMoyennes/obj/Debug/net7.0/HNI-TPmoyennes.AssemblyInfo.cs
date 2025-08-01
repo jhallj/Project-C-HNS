42238502d52642076708e67760861c91f06c43d3
jhallj <julien-hall@outlook.fr>
2025-08-01 10:52:27 +0200

    Je ne sais pas

--- JHA_TPMoyennes/obj/Debug/net7.0/HNI-TPmoyennes.AssemblyInfo.cs
+++ JHA_TPMoyennes/obj/Debug/net7.0/HNI-TPmoyennes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662f6a628df8bc910a7360277fea24a166032840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b45e5f73b82322b6fe5716d71e574911f8bd4f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
